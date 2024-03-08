9040c3b0a29ec3d9590d67990044a201e9a110fe
James Maloney <jamessagan@gmail.com>
2024-03-07 20:09:21 -0700

    add pomo module

---
+++ custom/pomo.sh
@@ -0,0 +1,9 @@
+show_pomo() {
+  local index=$1
+  local icon="$(get_tmux_option "@catppuccin_test_icon" "")"
+  local color="$(get_tmux_option "@catppuccin_test_color" "$thm_green")"
+
+  local module=$( build_status_module "$index" "$icon" "$color" "#{pomodoro_status}" )
+
+  echo "$module"
+}
